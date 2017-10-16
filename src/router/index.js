b768df3b1fa1977ea54e61e71020f1605f913a26
OneJ <3942241@qq.com>
2017-10-16 15:00:06 +0800

    +

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import PlanVC from '@/pages/planVC'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'PlanVC',
+      component: PlanVC
     }
   ]
 })
